ee26257eb912a73e9040505bae00c6bf068c971f
DhirajVishwakarma12 <dhirajvishwakarma51@gmail.com>
2026-07-20 01:10:45 +0530

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,39 +4,42 @@ dotenv.config()
 
 
 if(!process.env.MONGO_URI){
-    throw new Error("MONGO_URI not defined in enviroment variable")
+    throw new Error("MONGO_URI not defined in environment variable")
 }
 
 if(!process.env.FRONTEND_URL){
-    throw new Error("MONGO_URI not defined in enviroment variable")
+    throw new Error("FRONTEND_URL not defined in environment variable")
 }
 
 if(!process.env.JWT_SECRET){
-    throw new Error("JWT_SECRET not defined in enviroment variable")
+    throw new Error("JWT_SECRET not defined in environment variable")
 }
 
 if(!process.env.GOOGLE_CLIENT_ID){
-    throw new Error("GOOGLE_CLIENT_ID not defined in enviroment variable")
+    throw new Error("GOOGLE_CLIENT_ID not defined in environment variable")
 }
 
+
 if(!process.env.GOOGLE_CLIENT_SECRET){
-    throw new Error("GOOGLE_CLIENT_SECRET not defined in enviroment variable")
+    throw new Error("GOOGLE_CLIENT_SECRET not defined in environment variable")
 }
 
 if(!process.env.GOOGLE_REFRESH_TOKEN){
-    throw new Error("GOOGLE_REFRESH_TOKEN not defined in enviroment variable")
+    throw new Error("GOOGLE_REFRESH_TOKEN not defined in environment variable")
 }
-if(!process.env.GOOGEL_USER){
-    throw new Error("GOOGEL_USER not defined in enviroment variable")
+if(!process.env.GOOGLE_USER){
+    throw new Error("GOOGLE_USER not defined in environment variable")
 }
+
+
 
 const config ={
     MONGO_URI : process.env.MONGO_URI ,
     JWT_SECRET : process.env.JWT_SECRET,
-    GOOGLE_CLIENT_ID :process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET :process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_REFRESH_TOKEN :process.env.GOOGLE_REFRESH_TOKEN,
-    GOOGEL_USER :process.env.GOOGEL_USER,
+    GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID ,
+    GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET ,
+    GOOGLE_REFRESH_TOKEN : process.env.GOOGLE_REFRESH_TOKEN,
+    GOOGLE_USER : process.env.GOOGLE_USER,
     FRONTEND_URL:process.env.FRONTEND_URL
 }
 
