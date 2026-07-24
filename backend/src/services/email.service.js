b3b57add9247802a02f99a8f75fec09868703443
DhirajVishwakarma12 <dhirajvishwakarma51@gmail.com>
2026-07-25 01:29:02 +0530

    Update email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,7 +1,7 @@
-import nodemailer from "nodemailer";
 import { google } from "googleapis";
+import nodemailer from "nodemailer";
 import config from "../config/config.js";
-
+    
 const oAuth2Client = new google.auth.OAuth2(
     config.GOOGLE_CLIENT_ID,
     config.GOOGLE_CLIENT_SECRET
@@ -11,46 +11,28 @@ oAuth2Client.setCredentials({
     refresh_token: config.GOOGLE_REFRESH_TOKEN
 });
 
-export const sendemail = async (to, subject, text, html) => {
-    try {
-        const accessToken = await oAuth2Client.getAccessToken();
+const accessToken = await oAuth2Client.getAccessToken();
 
-        const transporter = nodemailer.createTransport({
-            host :"gmail",
-            port:465,
-            secure: true,
-            auth: {
-                type: "OAuth2",
-                user: config.GOOGLE_USER,
-                clientId: config.GOOGLE_CLIENT_ID,
-                clientSecret: config.GOOGLE_CLIENT_SECRET,
-                refreshToken: config.GOOGLE_REFRESH_TOKEN,
-                accessToken: accessToken.token,
-            },
-        });
 
-        await transporter.verify();
-        console.log("✅ SMTP Connected");
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+    type: "OAuth2",
+    user: config.GOOGLE_USER,
+    clientId: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    refreshToken: config.GOOGLE_REFRESH_TOKEN,
+    accessToken: accessToken.token,
+}
+});
 
-        const info = await transporter.sendMail({
-            from: `"To Do List" <${config.GOOGLE_USER}>`,
-            to,
-            subject,
-            text,
-            html,
-        });
+await transporter.verify();
+console.log("SMTP Ready");
 
-        console.log("✅ Email Sent:", info.messageId);
-
-        return info;
-    } catch (error) {
-        console.error("========== MAIL ERROR ==========");
-        console.error(error);
-        console.error("Message:", error.message);
-        console.error("Code:", error.code);
-        console.error("Response:", error.response);
-        console.error("===============================");
-
-        throw error;
-    }
-};
+await transporter.sendMail({
+    from: process.env.GOOGLE_USER,
+    to,
+    subject,
+    text,
+    html,
+});
