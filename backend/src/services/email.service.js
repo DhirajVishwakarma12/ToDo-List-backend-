85df90928e085ab6ee9941b52659c765875090b7
DhirajVishwakarma12 <dhirajvishwakarma51@gmail.com>
2026-07-25 11:40:18 +0530

    Update email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,38 +1,42 @@
-import { google } from "googleapis";
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
 import config from "../config/config.js";
-    
+
 const oAuth2Client = new google.auth.OAuth2(
     config.GOOGLE_CLIENT_ID,
     config.GOOGLE_CLIENT_SECRET
 );
 
 oAuth2Client.setCredentials({
-    refresh_token: config.GOOGLE_REFRESH_TOKEN
+    refresh_token: config.GOOGLE_REFRESH_TOKEN,
 });
 
-const accessToken = await oAuth2Client.getAccessToken();
+export const sendEmail = async (to, subject, text, html) => {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
 
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: config.GOOGLE_USER,
+                clientId: config.GOOGLE_CLIENT_ID,
+                clientSecret: config.GOOGLE_CLIENT_SECRET,
+                refreshToken: config.GOOGLE_REFRESH_TOKEN,
+                accessToken: accessToken.token,
+            },
+        });
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-    type: "OAuth2",
-    user: config.GOOGLE_USER,
-    clientId: config.GOOGLE_CLIENT_ID,
-    clientSecret: config.GOOGLE_CLIENT_SECRET,
-    refreshToken: config.GOOGLE_REFRESH_TOKEN,
-    accessToken: accessToken.token,
-}
-});
+        await transporter.sendMail({
+            from: config.GOOGLE_USER,
+            to,
+            subject,
+            text,
+            html,
+        });
 
-await transporter.verify();
-console.log("SMTP Ready");
-
-await transporter.sendMail({
-    from: process.env.GOOGLE_USER,
-    to,
-    subject,
-    text,
-    html,
-});
+        console.log("Email sent successfully");
+    } catch (err) {
+        console.error(err);
+    }
+};
