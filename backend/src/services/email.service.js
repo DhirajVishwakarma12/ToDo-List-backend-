e5c4ad594720af0c903e1945f6b51637b734d42c
DhirajVishwakarma12 <dhirajvishwakarma51@gmail.com>
2026-07-27 21:52:22 +0530

    Update email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,64 +1,42 @@
-import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
-
-import nodemailer from "nodemailer";
-import { google } from "googleapis";
-import config from "../config/config.js";
-
-const oAuth2Client = new google.auth.OAuth2(
-    config.GOOGLE_CLIENT_ID,
-    config.GOOGLE_CLIENT_SECRET
-);
-
-oAuth2Client.setCredentials({
-    refresh_token: config.GOOGLE_REFRESH_TOKEN,
-});
+import nodemailer from "nodemailer"
+import config from "../config/config.js"
 
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: config.GOOGEL_USER,
+        clientId: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        refreshToken: config.GOOGLE_REFRESH_TOKEN
+    }
+})
+
+//verify the connection configuration
+transporter.verify((error, info) => {
+    if (error) {
+        console.error("Error connecting to mail server:", error);
+    } else {
+        console.log("Email server is ready to send messages:", info);
+    }
+})
+
+//function to send email
 export const sendemail = async (to, subject, text, html) => {
     try {
-        const accessToken = await oAuth2Client.getAccessToken();
-        console.log(accessToken);
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
-             requireTLS: true,
-            auth: {
-                type: "OAuth2",
-                user: config.GOOGLE_USER,
-                clientId: config.GOOGLE_CLIENT_ID,
-                clientSecret: config.GOOGLE_CLIENT_SECRET,
-                refreshToken: config.GOOGLE_REFRESH_TOKEN,
-                accessToken: accessToken.token,
-            },
-        });
-            // check the transporter
-console.log("GOOGLE_USER:", config.GOOGLE_USER);
-
-try {
-    await transporter.verify();
-    console.log("SMTP Connected");
-} catch (err) {
-    console.error("VERIFY ERROR:", err);
-    throw err;
-}
-
-        await transporter.sendMail({
-            from: config.GOOGLE_USER,
+        const info = await transporter.sendMail({
+            from: `"To Do List" <${config.GOOGEL_USER}>`,
             to,
             subject,
             text,
             html,
         });
 
-        console.log("Email sent successfully");
-    } catch (err) {
-        console.error(err);
-        console.error("Message:", err.message);
-        console.error("Code:", err.code);
-        console.error("Stack:", err.stack);
-        console.error("Response:", err.response);
-        console.error("ResponseCode:", err.responseCode);
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        return info;
+    } catch (error) {
+        console.error("Error sending the message:", error);
     }
-};
+}
