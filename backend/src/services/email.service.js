411230a1fbff5f1a1c618543e7ff2d8d181df79b
DhirajVishwakarma12 <dhirajvishwakarma51@gmail.com>
2026-07-29 20:32:36 +0530

    Update email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,57 +1,43 @@
-import dns from "dns";
-
-dns.setDefaultResultOrder("ipv4first");
-import nodemailer from "nodemailer"
-import config from "../config/config.js"
-
-console.log(process.version);
+import nodemailer from "nodemailer";
+import config from "../config/config.js";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-
+    service: "gmail",
     auth: {
         type: "OAuth2",
-        user: config.GOOGEL_USER,
+        user: config.GOOGLE_USER,
         clientId: config.GOOGLE_CLIENT_ID,
         clientSecret: config.GOOGLE_CLIENT_SECRET,
         refreshToken: config.GOOGLE_REFRESH_TOKEN,
     },
+});
 
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
-});
-dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
-    console.log(err);
-    console.log(addresses);
-});
-//verify the connection configuration
-transporter.verify((error, info) => {
+await transporter.verify((error, success) => {
     if (error) {
-        console.error("Error connecting to mail server:", error);
+        console.log('Error connecting to email server:', error)
     } else {
-        console.log("Email server is ready to send messages:", info);
+        console.log('Email server is ready to send the message')
+
     }
 })
-
-//function to send email
 export const sendemail = async (to, subject, text, html) => {
     try {
         const info = await transporter.sendMail({
-            from: `"To Do List" <${config.GOOGEL_USER}>`,
+            from: `"To Do List" <${config.GOOGLE_USER}>`,
             to,
             subject,
             text,
             html,
         });
 
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        return info;
+        console.log("message send :%s", info.messageId)
+        console.log("Preview URL :%s", nodemailer.getTestMessageUrl(info))
+
+
     } catch (error) {
-        console.error("Error sending the message:", error);
+        console.log("Error sending email :%s", error)
+
     }
 }
+
+
